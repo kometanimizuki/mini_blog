4a39bafdd1706557a294232a6d300f45677c462e
kometanimizuki <rpslym1026@ezweb.ne.jp>
2019-11-14 21:13:17 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :userrails
   devise_for :users
   root 'tweets#index'
-  get 'tweets' => 'tweets#index'
-  get 'tweets/new' => 'tweets#new'
-  post 'tweets' => 'tweets#create'
-  delete  'tweets/:id'  => 'tweets#destroy'
-  get   'users/:id'   =>  'users#show'
+  get 'tweets', to: 'tweets#index'
+  get 'tweets/new', to: 'tweets#new'
+  post 'tweets', to: 'tweets#create'
+  delete  'tweets/:id', to: 'tweets#destroy'
+  get   'users/:id', to: 'users#show'
 end
